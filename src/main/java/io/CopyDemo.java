a90ff14ed63aefaa78bbf6b210cc2708e593d26b
Xiloer <83123095@qq.com>
2021-01-28 11:56:51 +0800

    第三天

---
+++ src/main/java/io/CopyDemo.java
@@ -0,0 +1,15 @@
+package io;
+
+/**
+ * 使用文件流完成文件的复制操作
+ */
+public class CopyDemo {
+    public static void main(String[] args) {
+        /*
+            1:创建文件输入流读取原文件
+            2:创建文件输出流写入复制文件
+            3:循环块读写完成复制
+            4:关闭两个流
+         */
+    }
+}
