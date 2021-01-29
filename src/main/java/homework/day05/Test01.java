a6125abd80715f82cc8197fdfbb7139de4a1d9b2
Zjc <1152926484@qq.com>
2021-01-29 13:09:59 +0800

    2021/01/29

--- src/main/java/homework/day05/Test01.java
+++ src/main/java/homework/day05/Test01.java
@@ -1,5 +1,7 @@
 package homework.day05;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -11,18 +13,16 @@ import java.io.IOException;
  */
 public class Test01 {
 	public static void main(String[] args) throws IOException {
-//		FileInputStream fis 
-//	= new FileInputStream("test.txt");
-//		FileInputStream fos
-//		= new FIleInputStream("test_cp.txt");
-//		
-//		   int d;
-//				while((d = fis.read())!=-1) {
-//				fos.write(fis.read());
-//					}
-//				System.out.println("复制完毕!");
-//					fis.close();
-//	fos.close();
+		FileInputStream fis = new FileInputStream("user.dat");
+		FileOutputStream fos = new FileOutputStream("user_cp.dat");
+           int len;
+		   byte[] data = new byte[1024*10];
+				while((len=fis.read(data))!=-1) {
+			      	fos.write(data,0,len);
+					}
+				System.out.println("复制完毕!");
+					fis.close();
+	             fos.close();
 	}
 }
 
--- src/main/java/homework/day05/Test02.java
+++ src/main/java/homework/day05/Test02.java
@@ -1,10 +1,6 @@
 package homework.day05;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.*;
 
 /**
  * 改正下面程序的错误
@@ -14,17 +10,22 @@ import java.io.FileOutputStream;
  *
  */
 public class Test02 {
-	public static void main(String[] args) throws FileNotFoundException {
-//			FileInputStream fis = new FileInputStream("test.txt");
-//		BufferedInputStream bis = new BuffereddInputStream(fis);
-//		
-//	FileOutputStream fos = new FileOutputStream("test_cp.txt");
-//			BufferedInputStream bos = new BufferedOutputStream(fos);
-//			
-//	int d =0;
-//	if((d = bis.read())==-1) {
-//		bos.wirte(d);
-//	}	
-//	System.out.println("复制完毕!");
+	public static void main(String[] args) throws IOException {
+			FileInputStream fis = new FileInputStream("user.dat");
+		    BufferedInputStream bis = new BufferedInputStream(fis);
+
+	        FileOutputStream fos = new FileOutputStream("user_cp02.dat");
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+	          int d;
+	          long start = System.currentTimeMillis();
+	          while((d = bis.read())!=-1) {
+//				  byte[] data = line.getBytes("UTF-8");
+	            	bos.write(d);
+	            	bos.flush();
+	          }
+		      long end = System.currentTimeMillis();
+	           System.out.println("复制完毕!耗时"+(end-start)+"毫秒");
+	           bos.close();
 	}
 }
