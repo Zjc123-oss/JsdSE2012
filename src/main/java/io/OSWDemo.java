c65f1a956df6078713069d032efba980fbf026f2
Xiloer <83123095@qq.com>
2021-01-29 10:44:36 +0800

    第六天

--- src/main/java/io/OSWDemo.java
+++ src/main/java/io/OSWDemo.java
@@ -14,6 +14,13 @@ import java.io.*;
  * 转换流:java.io.InputStreamReader和java.io.OutputStreamWriter
  * 转换流是常用的字符流实现类，它们是一对高级流，实际开发中我们不会直接
  * 使用它们，但是它们在流连接中是非常重要的一环。
+ *
+ * 因为其他高级字符流都有一个共同特点，在流连接中只能连接其他的字符流
+ * 而真实读写设备的低级流通常都是字节流，这会导致这些高级字符流不能直接
+ * 连接字节流。而转换流就在其中起到"转换器"的作用，它可以连接字节流，
+ * 并且它又是字符流，其他的字符流可以连接它。而它就负责将字符转换为字节。
+ *
+ *
  */
 public class OSWDemo {
     public static void main(String[] args) throws IOException {
---
+++ src/main/java/io/PWDemo2.java
@@ -0,0 +1,10 @@
+package io;
+
+/**
+ * 使用流连接形式创建PW完成文本数据的写出操作
+ */
+public class PWDemo2 {
+    public static void main(String[] args) {
+
+    }
+}
